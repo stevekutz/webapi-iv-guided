2ce10ee7419b2a04c976a6abeffc840cc0002dd4
stevekutz <stkutz@gmail.com>
2019-05-29 22:26:02 -0500

    set up env in index.j for heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./api/server.js');
-port = 4007;
-
+// port = 4007;
+const port = process.env.PORT || 4007;
 
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
