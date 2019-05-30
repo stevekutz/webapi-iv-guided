d584ecd053dcced726811b73c2cd6d46c7d0803b
stevekutz <stkutz@gmail.com>
2019-05-29 23:11:56 -0500

    added dotenv to index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/*
+const envReader = require('dotenv');
+envReader.config();  // reads .env and merges it into process.env
+*/
+// faster way
+require('dotenv').config();
+
+
 const server = require('./api/server.js');
 // port = 4007;
 const port = process.env.PORT || 4007;
